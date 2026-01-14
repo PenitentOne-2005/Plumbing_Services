24cee7827cc8931f4855a368dfd811e8e06aa090
PenitentOne-2005 <pozdrakovzena@gmail.com>
2026-01-14 23:16:14 +0300

    Add animation for img

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import styles from "./Hero.module.css";
 import container from "../../container.module.css";
 
 const Hero = () => {
+  const [loaded, setLoaded] = useState(false);
+
   return (
     <section className={styles.hero}>
       <div className={`${container.container} ${styles.inner}`}>
@@ -16,7 +19,12 @@ const Hero = () => {
         </div>
 
         <div className={styles.image}>
-          <img src="/Plumbing.webp" alt="Plumber" />
+          <img
+            src="/Plumbing.webp"
+            alt="Plumber"
+            onLoad={() => setLoaded(true)}
+            className={loaded ? styles.loaded : styles.img}
+          />
         </div>
       </div>
     </section>
